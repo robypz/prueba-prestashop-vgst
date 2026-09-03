186b67b167247754e76ee5c8a9e81413057fcef4
robypz <robertyepez0208@hotmail.com>
2026-09-03 13:22:56 +0200

    feat: initialize Falcon theme infrastructure and integrate WebP conversion utilities

---
+++ modules/is_shoppingcart/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'oksydan/is_shoppingcart',
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'reference' => '7ee4f275a2698068e7f7e935cb2a7a75c340b1b0',
+        'type' => 'prestashop-module',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'oksydan/is_shoppingcart' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'reference' => '7ee4f275a2698068e7f7e935cb2a7a75c340b1b0',
+            'type' => 'prestashop-module',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
